df3f2ad164b56cbea4a5d64f36aaf7105e2b1422
Rob Ballou <rob.ballou@gmail.com>
2011-12-27 15:13:52 -0600

    Made a few updates/bug fixes:
    1. Better hostname support: the code was only handling single hostnames (e.g., localhost), IP addresses, and domains like "example.com". Links to "example.co.uk" or "test.example.com" were being truncated incorrectly. Now we just specifically remove the "www" from a link if it is present.
    2. We now use jQuery.on() or jQuery.delegate() instead of jQuery.live().
    3. The code was refactored to put all of the tracking (external, internal) into a single function.
    4. The old window.console code was refactored to just use console.log and our detection of the console is more robust.
    5. Updated the readme

--- jquery.external-links-tracker.js
+++ jquery.external-links-tracker.js
@@ -1,6 +1,6 @@
 /*
   jQuery External Links Tracker Plugin
-  Copyright (C) 2010 Paradigm New Media Group
+  Copyright (C) 2011 Paradigm New Media Group
   http://pnmg.com | http://github.com/pnmg/jquery-external-links-tracker
 
   This program is free software; you can redistribute it and/or
@@ -19,44 +19,88 @@
 */
 (function($){
   $(document).ready(function(){
+    // detect if logging is enabled
+    var has_logger = typeof console === 'object' && console.log;
+    // detect if we can use the new jQuery.on() method
+  	var useOn = typeof jQuery.fn.on != 'undefined';
     // get the hostname, ignoring subdomains and the domain extension
     var host = window.location.hostname
-    host = host.split('.')
+    host = host.split('.');
     if(host.length == 1){ 
-      // localhost
-      host = host[0] 
+      // localhost, others?
+      host = host[0];
     }
     else if(host.length == 4 && window.location.hostname.match(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/)){ 
       // handle IPs
-      host = window.location.hostname 
+      host = window.location.hostname;
     }
     else {
       // domain names
-      host = host[host.length - 2] + '.' + host[host.length - 1] 
+      if(host[0] == 'www'){
+        host.shift(); // remove www
+      }
+      host = host.join('.');
     }
-    //check for internal links that need to open in a new window
-    $("a[href*='"+host+"'],a[href^='/'],a[href^='./'],a[href^='../']").live('click', function(e){
-      if($(this).hasClass("newwindow")) {
-        e.preventDefault();
-        if(window.console){ window.console.log('Internal link not tracked as outging: '+ $(this).attr('href')) };
-        window.open($(this).attr('href'));
+    var http = 'http://'+ host;
+    var https = 'https://'+ host;
+    
+    // handle the link tracking
+    var tracker = function(ev){
+      if(has_logger){ console.log('tracker()'); }
+      
+      var internalHandler = function(ev){
+        if($this.hasClass('newwindow')){
+          ev.preventDefault();
+          window.open(href);
+        }
       }
-    });
-    // track external links
-    $("a[href^='http']:not([href*='" + host + "'])").live('click', function(e){
-      e.preventDefault();
-      var linkLocation = this.href.replace(/^(http|https):\/\//, '').replace(/www\./i, '').replace(/\./gi, '_');
-      var track = '/outgoing/'+ linkLocation      
-      if(typeof(pageTracker) != 'undefined'){
-        pageTracker._trackPageview(track);
-        if(window.console){ window.console.log('Tracked external link: '+ track) };
-      } else if(typeof(_gat) == 'object') {
-        _gaq.push(['_trackPageview', track]);
-        if(window.console){ window.console.log('Tracked external link: '+ track) };
-      } else {
-        if(window.console){ window.console.log('Could not track external link: '+ track) }
+      
+      var $this = $(this);
+      var href = $this.attr('href');
+      if(/^[\.\/].+$/.test(href) && !/^\/\/.+/.test(href)){
+        // internal
+        if(has_logger){ console.log('internal'); }
+        internalHandler(ev);
       }
-      window.open($(this).attr('href'));
-    });
+      else {
+        // potentially external
+        if(href.substr(0, http.length) != http && href.substr(0, https.length) != https){
+          if(has_logger){ console.log('external'); }
+          ev.preventDefault();
+          var linkLocation = href.replace(/^(http|https):\/\//, '').replace(/www\./i, '').replace(/\/\//g, '').replace(/\./gi, '_');
+          var track = '/outgoing/'+ linkLocation;
+          if(typeof(pageTracker) != 'undefined'){
+            pageTracker._trackPageview(track);
+            if(has_logger){ console.log('Tracked external link: '+ track) };
+          } 
+          else if(typeof(_gat) == 'object') {
+            _gaq.push(['_trackPageview', track]);
+            if(has_logger){ console.log('Tracked external link: '+ track) };
+          } 
+          else {
+            if(has_logger){ console.log('Could not track external link: '+ track) }
+          }
+          window.open(href);
+        }
+        else {
+          if(has_logger){ console.log('internal 2'); }
+          internalHandler(ev);
+        }
+      }
+      
+    };
+    
+    // track links
+    var useOn = false;
+  	if(useOn){
+      // jQuery 1.7+
+      if(has_logger){ console.log('using .on()'); }
+      $('a[href]').on('click', tracker);
+  	}
+    else {
+      // jQuery < 1.7
+      if(has_logger){ console.log('using .delegate()'); }
+      $('body').delegate('a[href]', 'click', tracker);
+    }
   });
 })(jQuery);
